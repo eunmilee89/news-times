871a330575ded2943d0ac73a8c93841d536783b4
Eunmi Lee <eunoia0235@gmail.com>
2024-07-14 00:25:47 +0900

    pagination update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const API_KEY = `e63a0ba9a3634e71bd60f43254e6df47`; //newsapi_key
+const API_KEY = `e63a0ba9a3634e71bd60f43254e6df47`; //api_key
 let newsList=[];
 let userInput = document.getElementById("search-input");
 const menus = document.querySelectorAll(".menus button");
@@ -33,17 +33,26 @@ const openSearchBox = () => {
 
 let url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`);
 
+let totalResults = 0;
+let page =1;
+const pageSize = 10;
+const groupSize = 5;
+
 const getNews = async() => {
     try{
-        const response = await fetch(url);
+        url.searchParams.set("page",page); // => &page=page
+        url.searchParams.set("pageSize", pageSize); // => &pageSize=pageSize
 
-        const data = await response.json();
+        const response = await fetch(url);
+        let data = await response.json();
         if(response.status === 200){
             if(data.articles.length === 0){
                 throw new Error("There are no results that match your search");
             }
             newsList = data.articles;
+            totalResults = data.totalResults;
             render();
+            paginationRender();
         } else {
             throw new Error(data.message);
         }
@@ -55,20 +64,23 @@ const getNews = async() => {
 }
 
 const getNewsByCategory= async(event)=> {
+    page = 1;
     const category = event.target.textContent.toLowerCase();
     url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?category=${category}`);
-    getNews();
+    await getNews();
 }
 
 const getLatestNews = async()=>{
+    page = 1;
     url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`) 
-    getNews();
+    await getNews();
 }
 
 const searchNews = async() => {
+    page = 1;
     const keyword = document.getElementById("search-input").value;
     url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?q=${keyword}`);
-    getNews();
+    await getNews();
 }
 
 const render = () => {
@@ -103,6 +115,42 @@ const errorRender = (errorMessage) => {
     </div>`;
 
     document.getElementById("news-board").innerHTML = errorHtml;
+}
+
+const paginationRender = () => {
+    const totalPages = Math.ceil(totalResults/pageSize);
+    const pageGroup = Math.ceil(page/groupSize);
+    let lastPage = pageGroup * groupSize;
+    let paginationHTML = ``;
+
+    if(lastPage > totalPages){
+        lastPage = totalPages;
+    }
+    const firstPage = lastPage - (groupSize - 1)<= 0? 1 : lastPage - (groupSize - 1);
+
+    if(page > 1){
+        paginationHTML = `
+        <li class="page-item" onclick="moveToPage(1)"><a class="page-link" href="#">&lt;&lt;</a></li>
+        <li class="page-item" onclick="moveToPage(${page-1})"><a class="page-link" href="#">&lt;</a></li>`;
+    }
+    
+    for(let i=firstPage;i<=lastPage;i++){
+        paginationHTML += `<li class="page-item ${
+            i===page? "active":""
+        }" onclick="moveToPage(${i})"><a class="page-link" href="#">${i}</a></li>`;
+    }
+    if(page < totalPages){
+        paginationHTML += `
+        <li class="page-item" onclick="moveToPage(${page+1})" ><a class="page-link" href="#">&gt;</a></li>
+        <li class="page-item" onclick="moveToPage(${totalPages})" ><a class="page-link" href="#">&gt;&gt;</a></li>`
+    }
+    
+    document.querySelector(".pagination").innerHTML = paginationHTML;
+}
+
+const moveToPage = (pageNum) => {
+    page = pageNum;
+    getNews();
 }
 
 getLatestNews();
